c5e329b9ff96ef38c8f306ae7e20d9da62f97de9
narapon <43005911+narapons@users.noreply.github.com>
2021-08-03 00:29:17 +0900

    change

--- SimpleHome/src/czechpmdevs/simplehome/SimpleHome.php
+++ SimpleHome/src/czechpmdevs/simplehome/SimpleHome.php
@@ -47,6 +47,7 @@ class SimpleHome extends PluginBase {
     private $commands = [];
 
     public function onEnable() {
+        $this->getserver()->getPluginManager()->registerEvents($this, $this);
         self::$instance = $this;
         $this->registerCommands();
         $this->loadData();
@@ -86,7 +87,7 @@ class SimpleHome extends PluginBase {
      */
     public function getDisplayHomeList(Player $player): string {
         $list = $this->getHomeList($player);
-        $lv = MoneyLevelAPI:::getInstance()->getLv($player->getName());
+        $lv = MoneyLevelAPI:::getInstance()->getlv($player->getName());
         $max_lv = $this->messages["limit"] + ($lv / 100);
 
         if(count($list) == 0) {
@@ -118,7 +119,7 @@ class SimpleHome extends PluginBase {
      * @param Home $home
      */
     public function setPlayerHome(Player $player, Home $home) {
-        $lv = MoneyLevelAPI:::getInstance()->getLv($player->getName());
+        $lv = MoneyLevelAPI:::getInstance()->getlv($player->getName());
         $max_lv = $this->messages["limit"] + ($lv / 100);
         if($max_lv != -1) {
             if(count($this->getHomeList($player)) > $max_lv) {
